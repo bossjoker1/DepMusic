72ea067e6902f008132070db6681b64e9aa6ee24
bossjoker1 <1397157763@qq.com>
2020-09-08 23:11:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/depmusic/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.depmusic
+
+import android.app.Application
+
+class MyApplication :Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
---
+++ app/src/main/java/com/example/depmusic/activities/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.depmusic.activities
+
+import android.os.Bundle
+import com.example.depmusic.R
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
